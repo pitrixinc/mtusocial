d31b12e277e3e57b972b09b8fc4ad958aee0e0fe
Your name here <vidabonney278@gmail.com>
2023-09-02 12:06:24 +0000

    ...

--- components/Login.js
+++ components/Login.js
@@ -4,6 +4,7 @@ import { BsTwitter } from "react-icons/bs"
 import Image from 'next/image'
 import { FcGoogle } from "react-icons/fc"
 import Logo from "../assets/images/mtu.png";
+import Typewriter from "typewriter-effect";
 
 const Login = () => {
   return (
@@ -14,10 +15,22 @@ const Login = () => {
       </div>
 
       <div className='grid place-items-center'>
-
-        <div className='flex gap-4 bg-[#fff] p-4 px-6 items-center rounded-[6px] cursor-pointer' onClick={() => signIn('google')}>
+      <h1 className="font-bold text-2xl md:text-5xl lg:text-5xl p-5">
+         <span className="bg-gradient-to-r text-white from-yellow-500 to-black p-5 rounded-lg">Welcome To MTU Social</span>
+      </h1>
+      <h1 className="font-semibold text-xl bg-clip-text text-transparent bg-gradient-to-r from-yellow-500 to-black p-5 text-center">
+        <Typewriter 
+           options={{
+              strings: ['MTU Social: Where Huskies unite, passions ignite, and futures intertwine.','Connect with fellow Huskies; ignite your MTU journey today!','Unlock MTU`s vibrant community; share, inspire, and grow together.','Welcome to MTU Social: Where dreams meet and friendships flourish.','Step into MTU`s digital quad; discover, learn, and connect anew.','Join MTU`s digital campus; spark conversations, forge lifelong friendships.','MTU`s hub for connection: Explore, engage, and empower your aspirations.','Welcome, Huskies! Fuel your ambitions through shared experiences here.','Your gateway to MTU`s dynamic community: Connect, inspire, and excel.','Elevate your MTU experience: Unite, innovate, and thrive together.','Dear Husky, sign up to connect to the world of Tech'],
+              autoStart: true,
+              loop: true,
+           }}
+           />
+      </h1>
+         {/*  <h1 className="font-bold text-3xl">Dear Husky, sign up to connect to the world of Tech </h1> */}
+        <div className='flex gap-4 bg-[#fff] p-4 px-6 items-center rounded-[6px] cursor-pointer shadow-md' onClick={() => signIn('google')}>
           <FcGoogle className='text-[30px]' />
-          SignIn with Google
+          SignIn with Student Google Account
         </div>
 
       </div>
