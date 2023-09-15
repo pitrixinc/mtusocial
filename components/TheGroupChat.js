4059fc55443a6c2778f5f5b22aeb98b76892b6a8
Your name here <vidabonney278@gmail.com>
2023-09-15 13:07:07 +0000

    ...

--- components/TheGroupChat.js
+++ components/TheGroupChat.js
@@ -249,8 +249,8 @@ const TheGroupChat = () => {
 
   if (!isMember) {
     return <div className="flex justify-center items-center min-h-screen bg-gradient-to-r from-yellow-500 to-orange-500">
-              <p className="text-white text-xl text-center p-4 bg-gray-300 rounded-lg shadow-lg">
-                You are not a member of this group.
+              <p className="text-white text-2xl text-center p-4 bg-gray-300 rounded-lg shadow-lg">
+                You are not a member of this group. Go Back and Join
               </p>
             </div>;
   }
