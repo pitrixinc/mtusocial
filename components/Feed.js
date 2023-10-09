63c83c7a5edef89241fd33d52443e350fdc4b354
Your name here <vidabonney278@gmail.com>
2023-10-09 21:19:27 +0000

    ...

--- components/Feed.js
+++ components/Feed.js
@@ -122,6 +122,7 @@ const Feed = () => {
   function scrollToTop() {
       if (!isBrowser()) return;
       window.scrollTo({ top: 0, behavior: 'smooth' });
+      setIsNewPostsAvailable(false);
   }
 
   const showMorePosts = () => {
