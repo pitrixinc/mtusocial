22a521e481310bd6feab93877a64fa21f6176ee3
Your name here <vidabonney278@gmail.com>
2024-03-22 12:52:02 +0000

    ...

--- components/Feed.js
+++ components/Feed.js
@@ -11,7 +11,7 @@ import { toast } from 'react-toastify';
 import { BsNewspaper } from 'react-icons/bs';
 import { useRouter } from 'next/router'
 import Typewriter from "typewriter-effect";
-import { FaTimes } from 'react-icons/fa';
+import { MdClose } from "react-icons/md"
 
 
 const Feed = () => {
@@ -293,10 +293,10 @@ const [isMessageVisible, setMessageVisible] = useState(true);
            />
            </p>
            <button
-            className="absolute top-0 right-0 m-2 text-gray-500 hover:text-gray-700 focus:outline-none"
+            className="absolute top-0 right-0 m-2 text-gray-800 hover:text-gray-500 focus:outline-none"
             onClick={closeMessage}
           >
-            <FaTimes className="w-5 h-5" />
+            <MdClose className="text-[22px] cursor-pointer" />
           </button>
         </div>
       )}
