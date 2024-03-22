e9bb223ce71f97089f60fe8bf4435b93a57f8adf
Your name here <vidabonney278@gmail.com>
2024-03-22 11:13:08 +0000

    ...

--- components/Feed.js
+++ components/Feed.js
@@ -207,7 +207,7 @@ const Feed = () => {
          </div>
       </>) : (<>
       {isCurrentUserVerified && (<>
-        <p className="text-base text-center text-gray-800 leading-6 font-medium bg-gray-100 p-4 rounded-md shadow-md">
+        <p className="text-base text-center text-gray-800 leading-6 font-medium bg-gray-100 p-4 rounded-md shadow-sm">
         <Typewriter 
            options={{
             strings : [
